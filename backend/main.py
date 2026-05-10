a3a0e90a35aeb63470e54f53b2ac202e7798c167
yiannivarvaris <yvarvaris@gmail.com>
2026-05-10 20:47:53 +0930

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -3,14 +3,16 @@ from fastapi.responses import FileResponse
 import shutil
 import os
 
-from parser import extract_text_from_pdf, parse_races_from_text
+from parser import extract_text_from_pdf, extract_races
 from excel_writer import write_races_to_excel
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
     return {"message": "RISA Form Automation backend is running"}
+
 
 @app.post("/generate")
 async def generate_form(
@@ -30,11 +32,14 @@ async def generate_form(
         shutil.copyfileobj(excel_template.file, buffer)
 
     text = extract_text_from_pdf(risa_path)
-    races = parse_races_from_text(text)
 
-    print(f"Found {len(races)} races")
+    races = extract_races(text)
 
-    write_races_to_excel(template_path, output_path, races)
+    write_races_to_excel(
+        template_path,
+        output_path,
+        races
+    )
 
     return FileResponse(
         output_path,
